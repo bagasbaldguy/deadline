c3e8b2eabb84c9da0f5ad95b4b8bba3cfefed82a
bagasbaldguy <bagasbaldguy5@gmail.com>
2019-08-24 17:39:00 +0700

    Add Error Page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,4 +69,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+    public function showRegistrationform(){
+        return view('error.404');
+    }
 }
